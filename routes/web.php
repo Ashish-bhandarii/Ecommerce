9c3c2c93a514d65f9bd6bd7ba15402354cd85ca3
AshishBhandari <ashishhbhandri124@gmail.com>
2025-03-26 07:31:47 +0545

    create categories migration , model, controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,10 +10,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/categories', CategoryController::class);
+
+    // Route::get('/categories', CategoryController::class)->name('categories.index');
+    // Route::post('/categories', CategoryController::class)->name('categories.store');
+    // Route::put('/categories', CategoryController::class)->name('categories.edit');
+    // Route::delete('/categories', CategoryController::class)->name('categories.destroy');
+   
 });
 
 require __DIR__.'/auth.php';
